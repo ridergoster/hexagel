47cf9ed2cd9e9c551257a623efddb229f5830a92
Vincent Kocupyr <vincent.kocupyr@gmail.com>
2018-03-09 14:13:57 +0100

    update script & lib

--- hexagel.sh
+++ hexagel.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
+serverName="192.168.2.1"
+topicName="temperature_esgi"
+
 while true
   do
     tempA=$(mpl115a2 -a)
     temp=$(mpl115a2)
-    date=$(date)
+    date=$(date +"%y:%m:%d:%H:%M:%S")
+    message="$temp - $date"
 
     echo -n -e "\x76\x77\x02$tempA" | spi-pipe -d /dev/spidev0.0
 
-    mosquitto_pub -h 192.168.2.1 -t topic -m "$temp $date"
+    echo $message
+
+    mosquitto_pub -h $serverName -t $topicName -m $message
     sleep 1
 done
